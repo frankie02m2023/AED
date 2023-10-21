ffefbf7333e1c155bf84afaef5d0b07dd5029dc2
Lucas Faria <up202207540@up.pt>
2023-10-21 09:03:04 +0100

    Já funciona a class read_data_classes

--- testing1.cpp
+++ testing1.cpp
@@ -5,8 +5,8 @@
 
 int main(){
     interface interface1;
-    interface1.read_data_classes();
     interface1.read_data_classes_per_uc();
+    interface1.read_data_classes();
     interface1.print_data();
     return 0;
 }
